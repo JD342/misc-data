54fd8fc98b6435f442f322139aa8d68896b2f290
Nicola Fiori (JD342) <nicolafiori3@gmail.com>
2016-06-18 12:20:01 +0200

    First project commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace HelloWorld {
+	class MainClass {
+		public static void Main(string[] args) {
+			String name = "JD342";
+			Console.WriteLine("Hello World! I am " + name + "!");
+			Console.WriteLine("This is an attempt to do a C# program!");
+		}
+	}
+}
